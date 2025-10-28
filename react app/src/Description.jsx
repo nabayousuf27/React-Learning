61ca9c62f6deb98e51a58cbba6407b8ea9392f5d
Naba Yousuf <nabay0519@gmail.com>
2025-10-28 17:38:52 +0500

    creating first component

---
+++ react app/src/Description.jsx
@@ -0,0 +1,8 @@
+//component2
+function Description(){
+  return(
+    <p>hey i am paragraph tag in description fucntion component</p>
+  );
+}
+
+//export
---
+++ react app/src/Title.jsx
@@ -0,0 +1,10 @@
+//component 1
+function Title(){
+  return (
+    <div>
+      <h1>Hey i am title</h1>
+    </div>
+  );
+}
+
+//export
